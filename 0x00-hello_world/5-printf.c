1f42f34327255c349175e05e64799c9e678b632b
zingnkwa <thumbienkwawazingwe@gmail.com>
2023-03-16 13:25:25 -0700

    hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - main function
+*Return : successfuly executed (0)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
