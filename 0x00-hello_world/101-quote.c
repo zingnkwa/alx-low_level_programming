36a3a817123f08c8ebc817a153bdae1a95e20837
zingnkwa <thumbienkwawazingwe@gmail.com>
2023-03-17 08:38:57 -0700

    unix os

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a program that prints a line to a std error
+ * Return: always return 1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
