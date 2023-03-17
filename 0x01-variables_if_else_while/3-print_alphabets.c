9e30ca1a262c617fbaea6cf3d6d1c09bd7de8b22
zingnkwa <thumbienkwawazingwe@gmail.com>
2023-03-17 13:21:54 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	for (char i = 'a' ; i <= '2' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
