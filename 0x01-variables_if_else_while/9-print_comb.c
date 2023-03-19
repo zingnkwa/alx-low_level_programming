0d8697d6b0de776e7c42ec2463594a430ff31899
zingnkwa <thumbienkwawazingwe@gmail.com>
2023-03-19 02:15:16 -0700

    combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,15 @@ int main(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
+	{
 		if (i == 9)
 			putchar(i + '0');
 		else
+		{
 			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
-
+		}
+	}
 	return (0);
 }
