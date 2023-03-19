ce4e44815b72f0e1f7929c7df4f019c119bdc580
zingnkwa <thumbienkwawazingwe@gmail.com>
2023-03-19 01:18:02 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: returns a value
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		if (i == 9)
+			putchar(i + '0');
+		else
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+
+	return (0);
+}
