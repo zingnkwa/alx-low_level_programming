879d8f4a175a3c260383da65f4771e4a9e225fb4
zingnkwa <thumbienkwawazingwe@gmail.com>
2023-03-22 12:53:12 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: the first number
+ * @b: the second digit
+ * Return: the addition sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
